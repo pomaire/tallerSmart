ac4ddf765f31d544db204fb4a5164d53e3705486
pomaire <enanogludio@gmail.com>
2026-05-06 21:56:24 +0000

    cambios

---
+++ tallersmart-yii3/.merge-plan.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+// Do not edit. Content will be replaced.
+return [
+    '/' => [
+        'di' => [
+            'yiisoft/cache-file' => [
+                'config/di.php',
+            ],
+            'yiisoft/router-fastroute' => [
+                'config/di.php',
+            ],
+            'yiisoft/validator' => [
+                'config/di.php',
+            ],
+            'yiisoft/db' => [
+                'config/di.php',
+            ],
+            'yiisoft/router' => [
+                'config/di.php',
+            ],
+            'yiisoft/translator' => [
+                'config/di.php',
+            ],
+        ],
+        'params' => [
+            'yiisoft/cache-file' => [
+                'config/params.php',
+            ],
+            'yiisoft/db-migration' => [
+                'config/params.php',
+            ],
+            'yiisoft/log-target-file' => [
+                'config/params.php',
+            ],
+            'yiisoft/router-fastroute' => [
+                'config/params.php',
+            ],
+            'yiisoft/session' => [
+                'config/params.php',
+            ],
+            'yiisoft/validator' => [
+                'config/params.php',
+            ],
+            'yiisoft/db' => [
+                'config/params.php',
+            ],
+            'yiisoft/router' => [
+                'config/params.php',
+            ],
+            'yiisoft/translator' => [
+                'config/params.php',
+            ],
+        ],
+        'di-console' => [
+            'yiisoft/db-migration' => [
+                'config/di-console.php',
+            ],
+            'yiisoft/yii-console' => [
+                'config/di-console.php',
+            ],
+        ],
+        'common' => [
+            'yiisoft/log-target-file' => [
+                'config/common.php',
+            ],
+        ],
+        'di-web' => [
+            'yiisoft/router-fastroute' => [
+                'config/di-web.php',
+            ],
+            'yiisoft/session' => [
+                'config/di-web.php',
+            ],
+        ],
+        'events-console' => [
+            'yiisoft/yii-console' => [
+                'config/events-console.php',
+            ],
+            'yiisoft/log' => [
+                'config/events-console.php',
+            ],
+        ],
+        'params-console' => [
+            'yiisoft/yii-console' => [
+                'config/params-console.php',
+            ],
+        ],
+        'events-web' => [
+            'yiisoft/log' => [
+                'config/events-web.php',
+            ],
+            'yiisoft/middleware-dispatcher' => [
+                'config/events-web.php',
+            ],
+        ],
+    ],
+];
